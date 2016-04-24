10bce86324aee1bf297939b0925c0376df122f96
Alexander Pustovalov <apustovalov@gmail.com>
2016-04-24 17:28:48 +0300

    MUI theme provider for sandbox

--- .structor/sandbox/template/.structor/src/default.js
+++ .structor/sandbox/template/.structor/src/default.js
@@ -3,13 +3,19 @@ require("babel-polyfill");
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
 import PageForDesk from './PageForDesk.js';
 require('../../../../../src/client/assets/index.js');
 import initStore from '../../src/redux/store.js';
 
 ReactDOM.render(
-    <Provider store={initStore()}>
-        <PageForDesk />
-    </Provider>,
+    <MuiThemeProvider muiTheme={getMuiTheme()}>
+        <Provider store={initStore()}>
+            <PageForDesk />
+        </Provider>
+    </MuiThemeProvider>,
     document.getElementById('content')
 );
