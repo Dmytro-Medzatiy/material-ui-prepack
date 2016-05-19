f5c5de1301b96f1494142bfcce778313aafe996f
Alexander Pustovalov <apustovalov@gmail.com>
2016-05-20 00:13:33 +0300

    for Structor v1.0.7

--- .structor/sandbox/template/.structor/src/default.js
+++ .structor/sandbox/template/.structor/src/default.js
@@ -8,7 +8,7 @@ import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import PageForDesk from './PageForDesk.js';
-require('../../../../../src/client/assets/index.js');
+require('../../src/assets/index.js');
 import initStore from '../../src/redux/store.js';
 
 ReactDOM.render(
---
+++ .structor/sandbox/template/src/assets/index.js
@@ -0,0 +1,5 @@
+var injectTapEventPlugin = require('react-tap-event-plugin');
+injectTapEventPlugin();
+require('../../../../../src/client/assets/css/flexboxgrid.css');
+require('../../../../../src/client/assets/css/font-awesome.css');
+require('../../../../../src/client/assets/css/app.css');
